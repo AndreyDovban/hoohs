5a9f859171317225d12267ca77df84273b381a83
andrey <dovban.andreyy@rambler.ru>
2025-08-10 10:28:36 +0300

    Добавлена бивлиотека react-hot-toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import toast, { Toaster } from 'react-hot-toast';
+
+const notify = () => toast('Here is your toast.');
+
 function App() {
 	return (
 		<>
 			<h1>HELLO</h1>
+			<button onClick={notify}>Make me a toast</button>
+			<Toaster />
 		</>
 	);
 }
